94f0d0a4bd5d2ae096d49be7d455494ec759ca0f
Benjamin M Lancaster <bmlancaster07@gmail.com>
2018-11-08 22:42:09 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
-def square_array(array)
+numbers = [1,2,3]
 
+def square_array(array)
+  array.collect {|numbers| numbers * numbers}
+end 
+
+square_array(numbers)
+#USING COLLECT METHOD TO RETURN NEW ARRAY 
+
+def square_array(array)
+  new_arr = [] 
+  array.each do |numbers| 
+    square = numbers * numbers 
+    new_arr.push(square)
+ end
+  return new_arr
 end
+
+square_array(numbers)
